efa5f20a60f9e4bbd8a851f2d0663a9c684c6468
Delina Dvorzhetskaya <delina.dvorzhetskaya@gmail.com>
2021-11-18 18:14:01 +0300

    Initial Commit

---
+++ CatsImages/CatsImagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatsImagesApp.swift
+//  CatsImages
+//
+//  Created by Дэлина Дворжецкая on 18.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct CatsImagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
